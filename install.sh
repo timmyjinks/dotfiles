38e6601e8b90acb0b7b82ee909713aefca77ed35
timmy.jinks <lolimfat101@gmail.com>
2023-09-05 23:55:12 -0600

    add install script

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+#start 
+echo "Starting installation of dotfiles..."
+
+#bspwm
+mkdir $HOME/.config/bspwm
+cp -v $HOME/.config/bspwm/bspwmrc $HOME/.config/bspwm/
+
+#sxhkd
+mkdir $HOME/.config/sxhkd
+cp -v $HOME/dotfiles/.config/sxhkd/sxhkdrc $HOME/.config/sxhkd/
+
+#kitty
+mkdir $HOME/.config/kitty
+cp -v $HOME/dotfiles/.config/kitty/kitty.conf $HOME/.config/kitty/
+
+#polybar
+mkdir $HOME/.config/polybar
+cp -v $HOME/dotfiles/.config/polybar/config.ini $HOME/.config/polybar/
+cp -v $HOME/dotfiles/.config/polybar/launch.sh $HOME/.config/polybar/
