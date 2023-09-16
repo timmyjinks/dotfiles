0d0d4951e452898fc586d559a1d2d642e25ef980
timmy.jinks <lolimfat101@gmail.com>
2023-09-15 22:31:44 -0600

    add small changes to config files

--- install.sh
+++ install.sh
@@ -1,24 +1,29 @@
 #!/usr/bin/env bash
 
-#start 
+# start 
 echo "Starting installation of dotfiles..."
 
-#bspwm
+# bspwm
 mkdir $HOME/.config/bspwm
-cp -v $HOME/dotfiles/.config/bspwm/bspwmrc $HOME/.config/bspwm/
+chmod +x $HOME/.config/bspwm
+chmod +x $HOME/dotfiles/.config/autostart.sh
+cp -v $HOME/dotfiles/.config/bspwm/bspwmrc $HOME/.config/bspwm
+cp -v $HOME/dotfiles/.config/bspwm/autostart.sh $HOME/.config/bspwm
 
-#sxhkd
+# sxhkd
 mkdir $HOME/.config/sxhkd
-cp -v $HOME/dotfiles/.config/sxhkd/sxhkdrc $HOME/.config/sxhkd/
+cp -v $HOME/dotfiles/.config/sxhkd/sxhkdrc $HOME/.config/sxhkd
 
-#kitty
+# kitty
 mkdir $HOME/.config/kitty
-cp -v $HOME/dotfiles/.config/kitty/kitty.conf $HOME/.config/kitty/
+cp -v $HOME/dotfiles/.config/kitty/kitty.conf $HOME/.config/kitty
+cp -v $HOME/dotfiles/.config/kitty/colors.conf $HOME/.config/kitty
 
-#polybar
+# polybar
 mkdir $HOME/.config/polybar
-cp -v $HOME/dotfiles/.config/polybar/config.ini $HOME/.config/polybar/
-cp -v $HOME/dotfiles/.config/polybar/launch.sh $HOME/.config/polybar/
+chmod +x $HOME/dotfiles/.config/polybar/launch.sh
+cp -v $HOME/dotfiles/.config/polybar/config.ini $HOME/.config/polybar
+cp -v $HOME/dotfiles/.config/polybar/launch.sh $HOME/.config/polybar
 
 #end
 echo "...Installation complete"
