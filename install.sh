bda4a97bce14d3b38310e2676545e71e22b0b3f2
timmyjinks <tysonjjenkins.github@gmail.com>
2024-12-06 21:35:51 -0700

    Add nvim and update kitty

--- install.sh
+++ install.sh
@@ -3,27 +3,33 @@
 # start 
 echo "Starting installation of dotfiles..."
 
+# zsh
+cp -v $HOME/dotfiles/.zshrc $HOME/
+
+# nvim
+mkdir -p $HOME/.config/nvim
+cp -v $HOME/dotfiles/.config/nvim/* $HOME/.config/nvim
+
 # bspwm
-mkdir $HOME/.config/bspwm
-chmod +x $HOME/.config/bspwm
-chmod +x $HOME/dotfiles/.config/autostart.sh
-cp -v $HOME/dotfiles/.config/bspwm/bspwmrc $HOME/.config/bspwm
-cp -v $HOME/dotfiles/.config/bspwm/autostart.sh $HOME/.config/bspwm
+# mkdir $HOME/.config/bspwm
+# chmod +x $HOME/.config/bspwm
+# chmod +x $HOME/dotfiles/.config/autostart.sh
+# cp -v $HOME/dotfiles/.config/bspwm/bspwmrc $HOME/.config/bspwm
+# cp -v $HOME/dotfiles/.config/bspwm/autostart.sh $HOME/.config/bspwm
 
 # sxhkd
-mkdir $HOME/.config/sxhkd
-cp -v $HOME/dotfiles/.config/sxhkd/sxhkdrc $HOME/.config/sxhkd
+# mkdir $HOME/.config/sxhkd
+# cp -v $HOME/dotfiles/.config/sxhkd/sxhkdrc $HOME/.config/sxhkd
 
 # kitty
-mkdir $HOME/.config/kitty
-cp -v $HOME/dotfiles/.config/kitty/kitty.conf $HOME/.config/kitty
-cp -v $HOME/dotfiles/.config/kitty/colors.conf $HOME/.config/kitty
+mkdir -p $HOME/.config/kitty
+cp -v $HOME/dotfiles/.config/kitty/* $HOME/.config/kitty
 
 # polybar
-mkdir $HOME/.config/polybar
-chmod +x $HOME/dotfiles/.config/polybar/launch.sh
-cp -v $HOME/dotfiles/.config/polybar/config.ini $HOME/.config/polybar
-cp -v $HOME/dotfiles/.config/polybar/launch.sh $HOME/.config/polybar
+# mkdir $HOME/.config/polybar
+# chmod +x $HOME/dotfiles/.config/polybar/launch.sh
+# cp -v $HOME/dotfiles/.config/polybar/config.ini $HOME/.config/polybar
+# cp -v $HOME/dotfiles/.config/polybar/launch.sh $HOME/.config/polybar
 
 #end
 echo "...Installation complete"
